bc0768afdb9e134bb141eee70b818d4215703f21
serlahe <larahernandezsergio@gmail.com>
2026-01-26 21:08:49 -0300

    arreglo rutas de app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 ﻿const express = require('express');
 const cors = require('cors');
+const path = require('path');
+
 const authRoutes = require('./routes/auth.routes');
 const usuarioRoutes = require('./routes/usuario.routes');
 const rolRoutes = require('./routes/rol.routes');
@@ -9,33 +11,22 @@ const examenPacienteRoutes = require('./routes/examenPaciente.routes');
 const valorCriticoRoutes = require('./routes/valorCritico.routes');
 const avisoUrgenteRoutes = require('./routes/avisoUrgente.routes');
 const insumoRoutes = require('./routes/insumo.routes');
-const path = require('path');
-
-const seedRoles = require('./utils/seedRoles');
-const seedUsuarios = require('./utils/seedUsuarios');
-
-async function initSeeds() {
-    await seedRoles();
-    await seedUsuarios();
-}
-
-if (process.env.NODE_ENV !== 'production') {
-    initSeeds();
-}
-
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
+// RUTA AL FRONTEND
+const frontendPath = path.join(__dirname, '..', 'frontend', 'public');
+app.use(express.static(frontendPath));
 
+// Página inicial
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'dashboard.html'));
+    res.sendFile(path.join(frontendPath, 'login.html'));
 });
 
-// Rutas de API
+// Rutas API
 app.use('/api/auth', authRoutes);
 app.use('/api/auditoria', require('./routes/auditoria.routes'));
 app.use('/api/usuarios', usuarioRoutes);
@@ -47,10 +38,7 @@ app.use('/api/valores-criticos', valorCriticoRoutes);
 app.use('/api/avisos', avisoUrgenteRoutes);
 app.use('/api/insumos', insumoRoutes);
 
-
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`);
 });
-
